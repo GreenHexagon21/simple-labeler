dec8b42d4d86ee1eb285da94df0a0bfc3b745c1c
GreenHexagon21 <reppeti@gmail.com>
2024-08-21 16:26:38 +0200

    image toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'simple-labeler';
+  imagesEnabled = true;
+  constructor(private renderer: Renderer2) {}
 
   @HostListener('window:keydown', ['$event'])
   handleKeydown(event: KeyboardEvent): void {
@@ -15,9 +17,27 @@ export class AppComponent {
         this.hide();
         event.preventDefault();
         break;
+      case 'Insert':
+        this.toggleImages();
+        event.preventDefault();
+        break;
     }
   }
+
   hide() {
     window.location.href = 'http://localhost:4200/';
+  }
+
+  toggleImages() {
+    this.imagesEnabled = !this.imagesEnabled;
+    const images = document.getElementsByTagName('img');
+    for (let i = 0; i < images.length; i++) {
+      if (this.imagesEnabled) {
+        this.renderer.setStyle(images[i], 'display', 'block');
+      } else {
+        this.renderer.setStyle(images[i], 'display', 'none');
+      }
+     
+    }
   }
 }
